693d92568679d0b225e127ed8a84df8c61cf2cc7
Ranfees <153532316+Ranfees@users.noreply.github.com>
2025-11-23 16:03:58 +0530

    second commit

--- controller/auth.js
+++ controller/auth.js
@@ -4,38 +4,38 @@ exports.home = (req,res)=>{
 }
 
 exports.ethinic = (req,res)=>{
-    return res.render('ethinic')
+    return res.render('collections/ethinic')
 }
 
 exports.men = (req,res)=>{
-    return res.render('men')
+    return res.render('collections/men')
 }
 
 exports.women = (req,res)=>{
-    return res.render('women')
+    return res.render('collections/women')
 }
 
 exports.footwear = (req,res)=>{
-    return res.render('footwear')
+    return res.render('collections/footwear')
 }
 
 exports.loungewear = (req, res) => {
-    return res.render('loungewear');
+    return res.render('collections/loungewear');
 };
 
 
 exports.gift = (req,res)=>{
-    return res.render('gift')
+    return res.render('navbar-items/gift')
 }
 
 exports.zworld = (req,res)=>{
-    return res.render('zworld')
+    return res.render('navbar-items/zworld')
 }
 
 exports.zstories = (req,res)=>{
-    return res.render('zstories')
+    return res.render('navbar-items/zstories')
 }
 
 exports.zudioNear = (req,res)=>{
-    return res.render('zudioNear')
+    return res.render('navbar-items/zudioNear')
 }
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
 app.use(express.urlencoded())
 app.set('view engine', 'ejs')
+app.set('views','views') 
 
 app.use(express.static('static'));
 app.use('/style', express.static(__dirname + '/style'));
 
-
 const zudioRoutes = require('./routes/routes')
-app.use('/',zudioRoutes)
+app.use('/', zudioRoutes)
 
 app.listen(port)
